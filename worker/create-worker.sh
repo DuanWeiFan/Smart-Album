cef10837c2bf5282b1e809d0499a68d90b5a42c2
andyfan <andy.fan@changing.ai>
2019-12-06 22:42:18 -0700

    1. remove deprecated files/images, 2. add autoscaling image-worker command

--- worker/create-worker.sh
+++ worker/create-worker.sh
@@ -1,3 +1,4 @@
 PROJECT_ID=$(gcloud config get-value project 2> /dev/null)
 kubectl create deployment image-worker --image=gcr.io/"$PROJECT_ID"/project-worker:latest
 # kubectl expose deployment image-worker --port 50051 --target-port=50051
+kubectl autoscale deployment image-worker --cpu-percent=50 --min=1 --max=5
--- worker/woker-install.sh
+++
@@ -1,9 +0,0 @@
-apt-get update
-apt-get install -y python3 python3-pip
-pip3 install tensorflow keras grpcio
-pip3 install Pillow
-
-curl http://metadata/computeMetadata/v1/instance/attributes/worker -H "Metadata-Flavor: Google" > worker.py
-curl http://metadata/computeMetadata/v1/instance/attributes/img_pb2_grpc -H "Metadata-Flavor: Google" > img_pb2_grpc.py
-curl http://metadata/computeMetadata/v1/instance/attributes/img_pb2 -H "Metadata-Flavor: Google" > img_pb2.py
-python3 worker.py &
